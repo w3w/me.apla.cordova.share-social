d91d9f1a3b87a4202d79c1691509f90c6d3d25f2
Martin Hujer <mhujer@gmail.com>
2013-11-11 14:58:53 +0100

    ShareSocial for WP8

---
+++ src/wp8/ShareSocial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Phone.Tasks;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands
+{
+    class ShareSocial : BaseCommand
+    {
+        public void share(string options)
+        {
+            string[] jsOptions = JsonHelper.Deserialize<string[]>(options);
+            string url = jsOptions[2];
+            string heading = jsOptions[0];
+
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+
+            shareLinkTask.Title = heading;
+            shareLinkTask.LinkUri = new Uri(url, UriKind.Absolute);
+
+            shareLinkTask.Show();
+        }
+    }
+}
